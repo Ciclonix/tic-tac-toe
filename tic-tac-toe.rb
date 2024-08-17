8c7fa4fbbfaaaa45ec3be349b5425d634e6c7449
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2024-08-17 23:06:48 +0200

    Edit tic-tac-toe.rb, add add_symbol and check_win methods

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -4,7 +4,20 @@ class Grid
     def initialize
         @symbols = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
     end
-    
+
+    def add_symbol(x, y, symbol)
+        if (x.between? 0,2 and y.between? 0,2)
+            if symbols[y][x] == " "
+                symbols[y][x] = symbol
+                return true
+            end
+            puts "Error: Coordinates already used" 
+        else
+            puts "Error: Invalid coordinates"
+        end
+        return false
+    end
+
     def print_grid
         i = 0
         3.times do
@@ -14,5 +27,20 @@ class Grid
             puts " #{symbols[i][0]} | #{symbols[i][1]} | #{symbols[i][2]} "
             i += 1
         end
+    end
+
+    def check_win
+        for row in symbols
+            return true if row.uniq.size == 1 and row[0] != " "
+        end
+        col = 0
+        3.times do
+            return true if [symbols[0][col], symbols[1][col], symbols[2][col]].uniq.size == 1 and symbols[0][col] != " "
+            col += 1
+        end
+        return true if symbols[1][1] != " " and \
+                       ([symbols[0][0], symbols[1][1], symbols[2][2]].uniq.size == 1 or \
+                       [symbols[0][2], symbols[1][1], symbols[2][0]].uniq.size == 1)
+        return false
     end
 end
